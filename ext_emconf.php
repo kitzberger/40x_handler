0bd62c2727ba6a8b9fbf645024eb014f4706ac06
Philipp Kitzberger <philipp@kitze.net>
2020-10-28 16:05:51 +0100

    Bumped version

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,10 +10,10 @@ $EM_CONF[$_EXTKEY] = [
 	'author' => 'Philipp Kitzberger',
 	'author_email' => 'typo3@kitze.net',
 	'author_company' => '',
-	'version' => '1.1.0',
+	'version' => '1.2.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '7.6.0-9.5.99',
+			'typo3' => '9.5.0-10.4.99',
 		],
 		'conflicts' => [],
 		'suggests' => [],
